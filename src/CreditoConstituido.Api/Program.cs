5d7c67c81adedb2ab59c9f617446a5643e11e287
Matheus Szoke <Matheusszoke@gmail.com>
2025-12-15 20:42:16 -0300

    [feat] Criando Injeção de dependencia e aplicando-as no Program.cs da API

--- src/CreditoConstituido.Api/Program.cs
+++ src/CreditoConstituido.Api/Program.cs
@@ -1,25 +1,39 @@
+using CreditoConstituido.Api.HostedServices;
+using CreditoConstituido.Application;
+using CreditoConstituido.Infrastructure;
+using CreditoConstituido.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddHostedService<CreditoIntegrationWorker>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CreditoDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.MapControllers();
 
 app.Run();
+
+namespace CreditoConstituido.Api
+{
+    public class Program { }
+}
---
+++ src/CreditoConstituido.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using CreditoConstituido.Application.Interfaces;
+using CreditoConstituido.Infrastructure.Data;
+using CreditoConstituido.Infrastructure.Messaging;
+using CreditoConstituido.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CreditoConstituido.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<KafkaOptions>(configuration.GetSection("Kafka"));
+
+        services.AddDbContext<CreditoDbContext>(opt =>
+        {
+            var cs = configuration.GetConnectionString("Default");
+            opt.UseNpgsql(cs!);
+        });
+
+        services.AddSingleton(sp =>
+        {
+            var opts = sp.GetRequiredService<IOptions<KafkaOptions>>().Value;
+            return new ProducerBuilder<string, string>(new ProducerConfig
+            {
+                BootstrapServers = opts.BootstrapServers,
+                Acks = Acks.All,
+                EnableIdempotence = true
+            }).Build();
+        });
+
+        services.AddScoped<ICreditoRepository, CreditoRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<ICreditoIntegrationPublisher, KafkaCreditoIntegrationPublisher>();
+        services.AddScoped<ICreditoQueryAuditPublisher, KafkaCreditoQueryAuditPublisher>();
+
+        return services;
+    }
+}
