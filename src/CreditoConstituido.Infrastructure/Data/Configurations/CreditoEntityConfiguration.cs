eb7e30a43c166b90a3a40f2a65584f48a490f797
Matheus Szoke <Matheusszoke@gmail.com>
2025-12-15 20:41:39 -0300

    [feat] Definindo configurações da entidade Credito para banco de dados com EF Core

---
+++ src/CreditoConstituido.Infrastructure/Data/Configurations/CreditoEntityConfiguration.cs
@@ -0,0 +1,69 @@
+﻿using CreditoConstituido.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CreditoConstituido.Infrastructure.Data.Configurations;
+
+public sealed class CreditoEntityConfiguration : IEntityTypeConfiguration<Credito>
+{
+    public void Configure(EntityTypeBuilder<Credito> builder)
+    {
+        builder.ToTable("credito");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).HasColumnName("id");
+
+        builder.Property(x => x.NumeroCredito)
+            .HasColumnName("numero_credito")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.NumeroNfse)
+            .HasColumnName("numero_nfse")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.DataConstituicao)
+            .HasColumnName("data_constituicao")
+            .HasColumnType("date")
+            .IsRequired();
+
+        builder.Property(x => x.ValorIssqn)
+            .HasColumnName("valor_issqn")
+            .HasPrecision(15, 2)
+            .IsRequired();
+
+        builder.Property(x => x.TipoCredito)
+            .HasColumnName("tipo_credito")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.SimplesNacional)
+            .HasColumnName("simples_nacional")
+            .IsRequired();
+
+        builder.Property(x => x.Aliquota)
+            .HasColumnName("aliquota")
+            .HasPrecision(5, 2)
+            .IsRequired();
+
+        builder.Property(x => x.ValorFaturado)
+            .HasColumnName("valor_faturado")
+            .HasPrecision(15, 2)
+            .IsRequired();
+
+        builder.Property(x => x.ValorDeducao)
+            .HasColumnName("valor_deducao")
+            .HasPrecision(15, 2)
+            .IsRequired();
+
+        builder.Property(x => x.BaseCalculo)
+            .HasColumnName("base_calculo")
+            .HasPrecision(15, 2)
+            .IsRequired();
+
+        builder.HasIndex(x => x.NumeroCredito).IsUnique();
+        builder.HasIndex(x => x.NumeroNfse);
+    }
+}
---
+++ src/CreditoConstituido.Infrastructure/Data/CreditoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CreditoConstituido.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditoConstituido.Infrastructure.Data;
+
+public sealed class CreditoDbContext : DbContext
+{
+    public DbSet<Credito> Creditos => this.Set<Credito>();
+
+    public CreditoDbContext(DbContextOptions<CreditoDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CreditoDbContext).Assembly);
+    }
+}
