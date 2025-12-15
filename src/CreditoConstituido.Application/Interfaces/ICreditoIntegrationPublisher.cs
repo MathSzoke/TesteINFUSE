3334284aa08f3860826b8d8ac5b52ae64d23a9ab
Matheus Szoke <Matheusszoke@gmail.com>
2025-12-15 20:29:24 -0300

    [feat] Definindo contratos de métodos

---
+++ src/CreditoConstituido.Application/Interfaces/ICreditoIntegrationPublisher.cs
@@ -0,0 +1,8 @@
+﻿using CreditoConstituido.Application.Dtos;
+
+namespace CreditoConstituido.Application.Interfaces;
+
+public interface ICreditoIntegrationPublisher
+{
+    Task PublishAsync(IntegrarCreditoRequestItemDto credito, CancellationToken ct);
+}
