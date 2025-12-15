499850ad8ca246f0bff61213c409f98fcd4a6187
Matheus Szoke <Matheusszoke@gmail.com>
2025-12-15 20:57:05 -0300

    [fix] Adicionando correção de string da data para "yyyy/MM/dd"

---
+++ src/CreditoConstituido.Infrastructure/Data/Migrations/20251215205830_FixDataConstituicaoAsDate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreditoConstituido.Infrastructure.Data.Migrations
+{
+    public partial class FixDataConstituicaoAsDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "data_constituicao",
+                table: "credito",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "data_constituicao",
+                table: "credito",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
